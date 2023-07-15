5103ae16840afbe04b1b61e1ed089c09288d263b
pratyushsingha <pratyushsingha83@gmail.com>
2023-07-15 22:47:49 +0530

    fixing the error and working on it

--- script.js
+++ script.js
@@ -6,34 +6,36 @@ const profile_login = document.getElementById('profile_login')
 const profile_bio = document.getElementById('profile_bio')
 const profile_blog = document.getElementById('profile_blog')
 
-btn.addEventListener('click', () => {
-    const usernameVal = usernameText.value
-    console.log(usernameVal)
-    const p = fetch(`https://api.github.com/users/${usernameVal}`)
-    p.then((Response) => {
-        return Response.json()
-    }).then((data) => {
-        console.log(data)
-        results.textContent = `${data.name}`
-        profile_pic.src = data.avatar_url
-        profile_login.textContent = data.login
-        profile_bio.textContent = data.bio
-        profile_blog.href = data.blog 
-        profile_blog.textContent = data.blog
-        console.log(profile_blog.href)
-        usernameText.value = ""
+usernameText.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        const usernameVal = usernameText.value
+        console.log(usernameVal)
+        const p = fetch(`https://api.github.com/users/${usernameVal}`)
+        p.then((Response) => {
+            return Response.json()
+        }).then((data) => {
+            console.log(data)
+            results.textContent = `${data.name}`
+            document.title = `${data.name} (${data.name})`
+            profile_pic.src = data.avatar_url
+            profile_login.textContent = data.login
+            profile_bio.textContent = data.bio
+            profile_blog.href = data.blog
+            profile_blog.textContent = data.blog
+            console.log(profile_blog.href)
+            usernameText.value = ""
 
-    })
-        .catch(error => {
-            console.error("data cant be fetched")
         })
+            .catch(error => {
+                console.error("data cant be fetched")
+            })
         // const email = fetch(`https://api.github.com/users/${usernameVal}/events/public`)
         // email.then((Response) => {
         //     return Response.json()
         // }).then((data) => {
         //     console.log(data.)
-            
+
 
         // })
-
+    }
 })
